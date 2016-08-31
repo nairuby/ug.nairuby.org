5a5e58a260265665a0f3268551ed3fcc964bbd1b
banta <banta.bernard@gmail.com>
2016-08-31 15:07:54 +0300

    Revert "Changed repo url"
    This reverts commit 85b20d80e46e6cdd9ba0d1bce2754d8f7523ff35.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,6 @@
 lock '3.5.0'
 
 set :application, 'nairuby.org'
-set :repo_url, 'git@github.com:nairuby/ug.nairuby.git'
+set :repo_url, 'git@github.com:banta/nairuby.github.io.git'
 set :log_level, :debug
 set :rvm_ruby_version, '2.3.0@rails'
