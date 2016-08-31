884d5f83cd46a93164a9780add2c7c4ee8393a50
banta <banta.bernard@gmail.com>
2016-08-31 15:15:39 +0300

    Changed ruby version in travis and app name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'nairuby.org'
+set :application, 'ug.nairuby.org'
 set :repo_url, 'git@github.com:nairuby/ug.nairuby.org.git'
 set :log_level, :debug
 set :rvm_ruby_version, '2.3.0@rails'
